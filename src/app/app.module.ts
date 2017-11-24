762b024b1643835291c338f899ea651980bf07aa
pufi <pufanalexandru@gmail.com>
2017-11-24 18:51:07 +0200

    project init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { AccordionModule, ButtonModule, ChipsModule } from 'primeng/primeng';
 
 
 import { AppComponent } from './app.component';
@@ -10,7 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    AccordionModule,
+    ButtonModule,
+    ChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
